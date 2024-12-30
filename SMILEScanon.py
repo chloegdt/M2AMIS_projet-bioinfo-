4deced01aa7cd22a296c961098c204d623fe9823
clemenec <clemence.dumoulin24@gmail.com>
2024-12-30 23:27:35 +0100

    canonisation des smiles et verification avec l'inchkey

---
+++ SMILEScanon.py
@@ -0,0 +1,62 @@
+from rdkit import Chem
+import os
+
+dirname = "molecules_parsees"
+filename = "no_energy.mgf"
+
+#la je crée juste un dictionnaire avec les SMILES canonisés associé à leur INCHKEY, mais
+#mais peut-être qu'il faudra écrire le nouveau SMILE dans les fichiers .mgf
+#et aussi le code est brouillon parce que je galère trop à apprendre python :/
+
+def parse_into_dictionnary(dirname, filename):
+    inch_smiles = {}
+    for f in os.listdir(dirname):
+        if f != filename:
+            path = dirname + "/" + f
+            content = open(path, "r").readlines()
+            inch = None
+            smiles = None
+            #ce serait plus joli avec un regexp mais je suis vraiment un caca en python
+            for l in content:
+                #on ne verifie pas que les champs ne sont pas vides, pt il faut mettre des and
+                if l[0:8] == "INCHIKEY":
+                    inch = l[9:].replace("\n", "")
+                elif l[0:6] == "SMILES":
+                    smiles = l[7:].replace("\n","")
+                    inch_smiles[inch] = smiles
+        
+    return inch_smiles
+
+def canonisation(inch_smiles):
+    compt_diff_smiles = 0
+    for inch, smiles in inch_smiles.items():
+        mol = Chem.MolFromSmiles(smiles)
+
+        if mol is None:
+            inch_smiles[inch] = "SMILES invalide"
+        else:
+            tmp_smiles = Chem.MolToSmiles(mol)
+            if tmp_smiles != smiles:
+                compt_diff_smiles += 1
+            inch_smiles[inch] = tmp_smiles
+    return compt_diff_smiles
+
+def verif_with_inchkey(inch_smiles):
+    compt_smiles_invalides = 0
+    compt_diff_inch = 0
+    for inch, smiles in inch_smiles.items():
+        if smiles == "SMILES invalide":
+            compt_smiles_invalides += 1
+        else:
+            mol = Chem.MolFromSmiles(smiles)
+            inch_from_smile = Chem.inchi.MolToInchiKey(mol)
+            if inch_from_smile != inch:
+                compt_diff_inch += 1
+    return compt_smiles_invalides, compt_diff_inch
+
+inch_smiles = parse_into_dictionnary(dirname, filename)
+compt_diff_smiles = canonisation(inch_smiles)
+compt_smiles_invalides, compt_diff_inch = verif_with_inchkey(inch_smiles)
+print(f"nombre de smiles différents : {compt_diff_smiles}")
+print(f"nombre de smiles invalides : {compt_smiles_invalides}")
+print(f"nombre d'Inchikey différentes : {compt_diff_inch}")

9958c030fe76cf56fa8f313f3972e03b575fb715
Sébastien F <sebastien.fernandez0@gmail.com>
2025-01-28 12:48:03 +0100

    Ajout doc-string

--- fingerprint.py
+++ fingerprint.py
@@ -8,12 +8,17 @@ from rdkit.Chem import rdFingerprintGenerator
 from parse_data_final import create_dict_from_smiles
 
 
-FILENAME = "molecules_parsees/energy_102040_precursor_M+H.mgf"
+FILENAME = "cluster_molecules_test/smiles.txt"
 FPSIZE = 4096
 
 
 def getMolecules(filename):
-    """ Récupère les smiles du fichier et les transformes en molécules """
+    """
+    Récupère les smiles d'un fichier mgf et les transformes en objets rdkit molécule.
+    
+    @param filename: chemin du fichier mgf à lire.
+    @return: liste d'objets molécule.
+    """
     smiles = list()
     with mgf.MGF(filename) as reader:
         for molecule in reader:
@@ -21,19 +26,36 @@ def getMolecules(filename):
     return [Chem.MolFromSmiles(smile) for smile in smiles]
 
 def getMorganFingerprintsFromFile(filename):
-    """ Calcule les fingerprints de Morgan des molecules du fichier """
+    """
+    Calcule les fingerprints de Morgan des molecules à partir d'un fichier mgf.
+    
+    @param filename: chemin du fichier mgf à lire.
+    @return molecules: liste d'objets molécule.
+    @return fingerprints: liste de fingerprints (numpy array binaire).
+    """
     morganGen = rdFingerprintGenerator.GetMorganGenerator(radius=3, fpSize=FPSIZE)
     molecules = getMolecules(filename)
     return molecules, [np.array(morganGen.GetFingerprint(molecule)) for molecule in molecules]
 
 def getMorganFingerprints(smiles):
-    """ Calcule les fingerprints de Morgan à partir d'une liste de SMILES """
+    """
+    Calcule les fingerprints de Morgan à partir d'une liste de SMILES.
+    
+    @param smiles: liste de smiles.
+    @return : liste de fingerprints (numpy array binaire).
+    """
     molecules = [Chem.MolFromSmiles(smile) for smile in smiles]
     morganGen = rdFingerprintGenerator.GetMorganGenerator(radius=3, fpSize=FPSIZE)
     return [np.array(morganGen.GetFingerprint(molecule)) for molecule in molecules]
 
 def getRDkitFingerprintsFromFile(filename):
-    """ Calcule les fingerprints RDkit des molecules du fichier """
+    """
+    Calcule les fingerprints RDkit des molecules à partir d'un fichier mgf.
+    
+    @param filename: chemin du fichier mgf à lire.
+    @return molecules: liste d'objets molécule.
+    @return fingerprints: liste de fingerprints (numpy array binaire).
+    """
     rdkitGen = rdFingerprintGenerator.GetRDKitFPGenerator(minPath=1, maxPath=7, fpSize=FPSIZE)
     molecules = getMolecules(filename)
     return molecules, [np.array(rdkitGen.GetFingerprint(molecule)) for molecule in molecules]
@@ -41,8 +63,11 @@ def getRDkitFingerprintsFromFile(filename):
 
 def fingerprintsSimilarity(fingerprints):
     """
-        Calcule la similarité de Tanimoto entre chaque fingerprints
-        Tanimoto(A, B) = (A et B) / (A ou B)
+    Calcule la similarité de Tanimoto entre chaque fingerprints.
+    Tanimoto(A, B) = (A et B) / (A ou B)
+    
+    @param fingerprints: liste de fingerprints (numpy array binaire).
+    @return: matrice de similarité.
     """
     n_fp = len(fingerprints)
     similarity_matrix = np.zeros((n_fp, n_fp))
@@ -59,17 +84,28 @@ def fingerprintsSimilarity(fingerprints):
 
 
 def matrixToCSV(matrix, name):
-    """ Prend une liste de liste (matrice) en entrée et la sauvegarde au format csv dans le fichier similarity/nom.csv """
+    """
+    Sauvegarde une matrice dans un fichier .csv sous forme de matrice pleine.
+
+    @param matrix: matrice de similarité.
+    @param name: nom du fichier.
+    """
     folder = "similarity/"
     os.makedirs(folder, exist_ok=True)
     file = folder + name + '.csv'
 
     array = np.array(matrix)
     np.savetxt(file, array, fmt='%.6f', delimiter=',')
-    return file
 
 
 def matrixToTxt(matrix, name):
+    """
+    Sauvegarde une matrice dans un fichier .txt sous forme de matrice creuse.
+
+    @param matrix: matrice de similarité.
+    @param name: nom du fichier.
+    """
+    # TODO: changer nom folder et utiliser os.join path
     folder = "data/"
     os.makedirs(folder, exist_ok=True)
     file = folder + name + '.txt'
@@ -81,7 +117,11 @@ def matrixToTxt(matrix, name):
 
 
 def createEveryMatrix(filename):
-    """ Créé toutes les matrices de similarité """
+    """
+    Calcule et sauvegarde (en fichier CSV) les matrices de similarité à partir d'un fichier .txt.
+    
+    @param filename: chemin du fichier .txt à lire.
+    """
     smiles_dict = create_dict_from_smiles(filename)
     for filename, smiles in smiles_dict.items():
         fg = getMorganFingerprints(smiles)
@@ -91,11 +131,10 @@ def createEveryMatrix(filename):
 
 
 if __name__ == '__main__':
-    molecules, fg = getMorganFingerprintsFromFile("cluster_molecules_test/energy_25.0_precursor_M+H.mgf")
-    # molecules, fg = getRDkitFingerprints(FILENAME)
-    fg_sim = fingerprintsSimilarity(fg)
-    matrixToTxt(fg_sim, "fingerprints_energy_25.0_precursor_M+H.mgf")
-    print(fg_sim)
+    file_test = "cluster_molecules_test/energy_100.0_precursor_M+H.mgf"
 
-    # print(sorted(fg_sim[-1],reverse=False))
-    # createEveryMatrix("cluster_molecules_test/smiles.txt")
+    molecules, fg = getMorganFingerprintsFromFile(file_test)
+    fg_sim = fingerprintsSimilarity(fg)
+    matrixToTxt(fg_sim, "fingerprints_" + file_test.split("/")[1])
+
+    # createEveryMatrix(FILENAME)

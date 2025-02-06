1628467c73030a22a7f39594c2068a6db4697766
Rafifoulol <raph07.guerin@gmail.com>
2025-02-06 18:47:47 +0100

    Pourcentage de molecules communes

--- shared_cluster.py
+++ shared_cluster.py
@@ -1,5 +1,6 @@
 import networkx as nx
 from pyvis.network import Network
+from pyteomics import mgf
 
 def load_clusters(file_path):
     
@@ -21,6 +22,28 @@ def find_common_clusters(smiles_clusters, spectrum_clusters):
                 common_clusters[(smiles_id, spectrum_id)] = list(common_molecules)
 
     return common_clusters
+
+def molecules_pourcentage(file, cluster_spectre, cluster_smiles) :
+
+    total_count = 0 
+    with mgf.MGF(file) as spectres:
+        for spectre in spectres:
+            total_count += 1
+
+    spectrum_molecules = set(mol for cluster in cluster_spectre.values() for mol in cluster)
+    smiles_molecules = set(mol for cluster in cluster_smiles.values() for mol in cluster)
+
+    shared_molecules = spectrum_molecules & smiles_molecules
+    total_unique_molecules = spectrum_molecules | smiles_molecules
+
+    percentage = (len(shared_molecules) / len(total_unique_molecules)) * 100 if total_unique_molecules else 0
+
+    print(f"Nombre Total de molécules : ", total_count)
+    print(f"Nombre de molécules dans les Clusters de Spectre : {len(spectrum_molecules)}")
+    print(f"Nombre de molécules dans les Clusters de SMILES : {len(smiles_molecules)}")
+    print(f"Molécules communes : {len(shared_molecules)}")
+    print(f"Pourcentage de molécules communes : {percentage:.2f}%")
+    
 
 def plot_common_clusters(common_clusters, output_file="common_clusters.html"):
     
@@ -47,18 +70,16 @@ def plot_common_clusters(common_clusters, output_file="common_clusters.html"):
         net.add_edge(edge[0], edge[1], color="gray")
     net.write_html(output_file)
 
-    print(f"Network saved as {output_file}. Open it manually in your browser.")
+    print(f"Network saved as {output_file}. Ouvrez le fichier manuellement (firefox common_clusters).")
 
+file = "energy_37.0_precursor_M+Na.mgf"
+file1 = "cluster_smiles_37.0_precursor_M+Na.txt"
+file2 = "cluster_spectre_37.0_precursor_M+Na.txt"
 
-smiles_file = "resultats_clusters_dbscan/clusters_smiles_dbscan/energy_10.0_precursor_M+H.txt"
-spectrum_file = "resultats_clusters_dbscan/clusters_spectres_dbscan/energy_10.0_precursor_M+H.txt"
-
-file1 = ""
-file2 = ""
-
-smiles_clusters = load_clusters(smiles_file)
-spectrum_clusters = load_clusters(spectrum_file)
+smiles_clusters = load_clusters(file1)
+spectrum_clusters = load_clusters(file2)
 
 common_clusters = find_common_clusters(smiles_clusters, spectrum_clusters)
 
 plot_common_clusters(common_clusters)
+molecules_pourcentage(file, spectrum_clusters, smiles_clusters)
